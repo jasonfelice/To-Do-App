c19c79f1a57520d92f57c910f55bc8b37881b79c
Jake Felice <jasonfelice0@gmail.com>
2022-05-31 05:53:12 -0700

    Refactor all components to functional components

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,74 +1,63 @@
 /* eslint-disable */
 
-import React from 'react';
-import styles from './TodoItem.module.css';
+import React, { useState, useEffect } from "react"
+import styles from "./TodoItem.module.css"
 
-class TodoItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      editing: false,
-    };
+const TodoItem = props => {
+  const [editing, setEditing] = useState(false);
+
+  const handleEditing = () => {
+    setEditing(true)
   }
 
-  handleEditing = () => {
-    this.setState({
-      editing: true,
-    });
-  }
-
-  handleUpdatedDone = event => {
+  const handleUpdatedDone = event => {
     if (event.key === "Enter") {
-      this.setState({ editing: false });
+      setEditing(false)
     }
   }
 
-  render() {
-    const completedStyle = {
-      fontStyle: 'italic',
-      color: '#595959',
-      opacity: 0.4,
-      textDecoration: 'line-through',
-    };
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  }
 
-    const { completed, id, title } = this.props.todo;
+  const { completed, id, title } = props.todo
 
-    const viewMode = {};
-    const editMode = {};
+  let viewMode = {}
+  let editMode = {}
 
-    if (this.state.editing) {
-      viewMode.display = 'none';
-    } else {
-      editMode.display = 'none';
-    }
+  if (editing) {
+    viewMode.display = "none"
+  } else {
+    editMode.display = "none"
+  }
 
-    return (
-      <li className={styles.item}>
-        <div onDoubleClick={this.handleEditing} style={viewMode}>
-          <input
-            type="checkbox"
-            className={styles.checkbox}
-            checked={completed}
-            onChange={() => this.props.handleChangeProps(id)}
-          />
-          <button type='button' onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
-          <span style={completed ? completedStyle : null}>
-            {title}
-          </span>
-        </div>
+  return (
+    <li className={styles.item}>
+      <div onDoubleClick={handleEditing} style={viewMode}>
         <input
-          type="text"
-          className={styles.textInput}
-          style={editMode}
-          value={title}
-          onChange={(e) => {
-            this.props.setUpdate(e.target.value, id)
-          }}
-          onKeyDown={this.handleUpdatedDone}
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => props.handleChangeProps(id)}
         />
-      </li>
-    );
-  }
+        <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
+        <span style={completed ? completedStyle : null}>{title}</span>
+      </div>
+      <input
+        type="text"
+        style={editMode}
+        className={styles.textInput}
+        value={title}
+        onChange={e => {
+          props.setUpdate(e.target.value, id)
+        }}
+        onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  )
 }
 
-export default TodoItem;
+export default TodoItem
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,27 +1,25 @@
-/* eslint-disable */
-
-import React, { Component } from 'react';
+/* eslint-disable react/prop-types */
+import React from 'react';
 import TodoItem from './TodoItem';
 
-export default class TodosList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Component = (props) => {
+  const {
+    todos, handleChangeProps, deleteTodoProps, setUpdate,
+  } = props;
 
-  render() {
-    const { todos } = this.props;
-    return (
-      <ul>
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            handleChangeProps={this.props.handleChangeProps}
-            deleteTodoProps={this.props.deleteTodoProps}
-            setUpdate={this.props.setUpdate} />
-        ))}
-      </ul>
-    );
-  }
-}
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={handleChangeProps}
+          deleteTodoProps={deleteTodoProps}
+          setUpdate={setUpdate}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default Component;
